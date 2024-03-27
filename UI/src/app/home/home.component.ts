02e6944ca8dafebe849cc50de11ae21361c86bed
logically positive <brianizquierdo0229@gmail.com>
2024-03-27 11:40:35 -0400

    everything works :)

--- UI/src/app/home/home.component.ts
+++ UI/src/app/home/home.component.ts
@@ -3,19 +3,23 @@ import { HomeService } from './home.service';
 import { AuthService } from '../auth/auth.service';
 import { HttpClient } from '@angular/common/http';
 import { Subscription, take } from 'rxjs';
+import { NgIf } from '@angular/common';
 
 // gets data from home service and displays it
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [NgIf],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
 export class HomeComponent {
   information = 'Welcome to the home page!';
   username: string | null = null;
+  selectedSpecFile: File | null = null;
+  selectedFlatFile: File | null = null;
   private userSub: Subscription | null = null;
+  mergedData: string | null = null;
 
   constructor(
     private http: HttpClient,
@@ -34,5 +38,35 @@ export class HomeComponent {
 
   getUsers() {
     this.homeService.getData();
+  }
+
+  onFlatFileSelected(event: any) {
+    this.selectedFlatFile = <File>event.target.files[0];
+    console.log('Selected: ', this.selectedFlatFile);
+  }
+  onSpecFileSelected(event: any) {
+    this.selectedSpecFile = <File>event.target.files[0];
+    console.log('Selected: ', this.selectedSpecFile);
+  }
+  submitFlatFile() {
+    console.log('Submitted Flat File: ', this.selectedFlatFile);
+    if (this.selectedFlatFile) {
+      this.homeService.submitFlatFile(this.selectedFlatFile);
+    }
+  }
+  submitSpecFile() {
+    console.log('Submitted Spec File: ', this.selectedSpecFile);
+    if (this.selectedSpecFile) {
+      this.homeService.submitSpecFile(this.selectedSpecFile);
+    }
+  }
+
+  submitMerge() {
+    if (this.selectedFlatFile && this.selectedSpecFile) {
+      this.homeService.submitMerger(
+        this.selectedFlatFile?.name,
+        this.selectedSpecFile?.name
+      );
+    }
   }
 }
--- UI/src/app/home/home.service.ts
+++ UI/src/app/home/home.service.ts
@@ -1,21 +1,87 @@
 import { Injectable } from '@angular/core';
 import { AuthService } from '../auth/auth.service';
-import { HttpClient } from '@angular/common/http';
-import { Subscription } from 'rxjs';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { Observable, Subscription } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class HomeService {
+  mergedData: string | null = null;
   constructor(private http: HttpClient, private authService: AuthService) {}
 
   getData() {
     this.authService.user.subscribe((user) => {
       if (user) {
+        const headers = new HttpHeaders({
+          Authorization: user.token as string,
+        });
         this.http
           .get('http://localhost:8080/users', {
-            headers: {
-              Authorization: user.token as string,
-            },
+            headers: headers,
           })
+          .subscribe((data) => {
+            console.log(data);
+          });
+      }
+    });
+  }
+  submitFlatFile(file: File) {
+    this.authService.user.subscribe((user) => {
+      if (user) {
+        const formData = new FormData();
+        formData.append('file', file);
+        const headers = new HttpHeaders({
+          Authorization: user.token as string,
+        });
+        this.http
+          .post('http://localhost:8080/flatfiles', formData, {
+            headers: headers,
+            responseType: 'text',
+          })
+          .subscribe((data) => {
+            console.log(data);
+          });
+      }
+    });
+  }
+
+  submitSpecFile(file: File) {
+    this.authService.user.subscribe((user) => {
+      if (user) {
+        const formData = new FormData();
+        formData.append('file', file);
+        const headers = new HttpHeaders({
+          Authorization: user.token as string,
+        });
+        this.http
+          .post('http://localhost:8080/specfiles', formData, {
+            headers: headers,
+            responseType: 'text',
+          })
+          .subscribe((data) => {
+            console.log(data);
+          });
+      }
+    });
+  }
+
+  submitMerger(flatFileName: string, specFileName: string) {
+    this.authService.user.subscribe((user) => {
+      if (user) {
+        const headers = new HttpHeaders({
+          Authorization: user.token as string,
+        });
+        let params = new HttpParams();
+        params = params.append('flatFileName', flatFileName);
+        params = params.append('specFileName', specFileName);
+        this.http
+          .post(
+            'http://localhost:8080/mergefiles',
+            {},
+            {
+              headers: headers,
+              params: params,
+            }
+          )
           .subscribe((data) => {
             console.log(data);
           });
